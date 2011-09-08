4f4ecfadf077a2474f8bb64fbb98ed3404db026f
Babar K. Zafar <babar.zafar@gmail.com>
2011-09-08 02:05:02 +0200

    Build standalone DropboxURI.app via py2app.

--- setup.py
+++ setup.py
@@ -1,19 +1,60 @@
-import sys
+import os, sys
 
-from distutils.core import setup
+from setuptools import setup
 
-if sys.platform == "win32":
+###########################################################################
+# Global configuration.
+###########################################################################
+
+global_options = dict(
+    name = "DropboxURI",
+    version = "0.1",
+    description = "Utility for sharing URIs for items in shared Dropbox folders.",
+    author = "Babar K. Zafar",
+    author_email = "babar.zafar@gmail.com",
+    url = "https://github.com/bkz",
+    long_description = "n/a",
+)
+
+###########################################################################
+# Target OSX.
+###########################################################################
+
+def build_darwin():
+    import py2app
+
+    sys.argv.append('py2app')
+
+    plist = dict(
+       CFBundleURLTypes = [
+            dict(CFBundleURLSchemes = ["dropbox"],
+                 CFBundleURLName = "Open dropbox files via uri.")
+        ],
+    )
+
+    setup(
+        app=["main.py"],
+        options = {
+            "py2app": {
+                "argv_emulation" : True,
+                "iconfile"       : "dropbox.icns",
+                "plist"          : plist,
+            }
+        },
+        **global_options
+    )
+
+
+###########################################################################
+# Target Windows.
+###########################################################################
+
+def build_windows():
     import py2exe
+
     sys.argv.append('py2exe')
 
     setup(
-        name = "Dropbox Copy URI",
-        version = "0.1",
-        description = "Utility for sharing URIs for items in shared Dropbox folders.",
-        author = "Babar K. Zafar",
-        author_email = "babar.zafar@gmail.com",
-        url = "https://github.com/bkz",
-        long_description = "n/a",
         windows = [{"dest_base": "dropbox", "script": "main.py"}],
         data_files = [("", ["dropbox.ico", "platform.dll"])],
         zipfile = None,
@@ -26,6 +67,22 @@ if sys.platform == "win32":
                 "dist_dir"     : 'bin',
             }
         },
+        **global_options
     )
+
+
+###########################################################################
+# Trigger platform specific build.
+###########################################################################
+
+if sys.platform == "darwin":
+    build_darwin()
+elif sys.platform == "win32":
+    build_windows()
 else:
-    raise RuntimeError("Unsupported platform")
+    raise NotImplementedError("Unsupported platform")
+
+
+###########################################################################
+# The End.
+###########################################################################
